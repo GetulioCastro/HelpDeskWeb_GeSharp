1d7ad0de0e8f8a526a81aadfc5122d4bbfed8f8b
IM <85082822+molin6@users.noreply.github.com>
2023-09-23 14:41:53 -0500

    upload to github from gitlab

---
+++ Hweb/data/hwebDBcontext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hweb;
+    
+namespace Hweb.data
+{
+    public class hwebDBcontext : DbContext
+    {
+        public hwebDBcontext(DbContextOptions<hwebDBcontext> options) : base(options) 
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<userRequests>()
+                .HasKey(c => new { c.requestID, c.username });
+        }
+
+        public DbSet<Login_Credentials> Login_Credentials { get; set; }
+
+        public DbSet<Hweb.Requests>? Requests { get; set; }
+        public DbSet<Hweb.userRequests>? userRequests { get; set; }
+        public DbSet<Hweb.completed_request_log>? completed_request_log { get; set; }
+    }
+}
